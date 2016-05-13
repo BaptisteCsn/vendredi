9d4e95a28b6ea801d97c77ef82b6ff72343e9f83
Baptiste <baptistecousin78@gmail.com>
2016-05-13 11:33:13 +0200

    page formulaire
    page formulaire

---
+++ form.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+
+        <title>Coucou</title> 
+        <link rel="stylesheet" href="css/form.css"
+
+              </head>
+
+        <body>
+            <div id="wrapper">
+                <header>Holaa</header>
+                <div id="inner-content">
+
+                    <main>
+                        <div class="moins480">Taille écran inférieure à 480px</div>
+                        <div class="plus480">Taille écran supérieure à 480px</div>
+                        <form action="#" method="get"><br>      
+
+                            <label for="firstname">Nom :</label>
+                            <input type="text" name="firstname"><br><br> 
+
+                            <label for="email">Email :</label>
+                            <input type="email" name="mail"><br><br>
+
+                            <input type="file"><br><br>
+                            <input type="reset" value="Recommencer">            
+                            <input type="submit" value="Envoyer">
+
+                        </form>
+                    </main>
+                    <aside>Contenu Barre Latérale</aside>
+                </div>
+
+                <footer>
+                    <div id="btMenu">
+                        <a href="#">Menu</a>
+                    </div><div id="btAccueil">
+                    <a href="#">Accueil</a>
+                    </div>    
+                </footer>
+            </div>
+        </body>
+        </html>
